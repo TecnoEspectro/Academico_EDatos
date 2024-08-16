61b9d9eecb5315fa165ca551491bd2d971f5b377
Tecno Espectro <TecnoEspectro15@hotmail.com>
2024-08-16 08:10:27 -0500

    clase

---
+++ Academico/src/main/java/com/mycompany/academico/Academico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.academico;
+
+/**
+ *
+ * @author SCIS2-13
+ */
+public class Academico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
